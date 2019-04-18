9ae4496cea4fe84b78ba7e8b353e5fe778cbd403
Romain Warnan <romain.warnan@gmail.com>
2019-04-18 11:38:49 +0200

    Ex2, b

--- src/main/kotlin/com/paulienvanalst/rugbymatch/events/GameEvents.kt
+++ src/main/kotlin/com/paulienvanalst/rugbymatch/events/GameEvents.kt
@@ -4,6 +4,8 @@ import com.paulienvanalst.rugbymatch.game.SetPiece
 import com.paulienvanalst.rugbymatch.team.TeamName
 import org.springframework.context.ApplicationEvent
 
-sealed class SetPieceEvent(source: Any, val setPiece: SetPiece, val winningTeam: TeamName) : ApplicationEvent(source)
+sealed class SetPieceEvent(source: Any, open val setPiece: SetPiece, open val winningTeam: TeamName) : ApplicationEvent(source)
 
+class ScrumWasPlayed(source: Any, override val setPiece: SetPiece, override val winningTeam: TeamName): SetPieceEvent(source, setPiece, winningTeam)
 
+class LineOutWasPlayed(source: Any, override val setPiece: SetPiece, override val winningTeam: TeamName): SetPieceEvent(source, setPiece, winningTeam)
