8230ece7c3f1836977ddbe808f76d04da1fbffc1
Romain Warnan <romain.warnan@gmail.com>
2019-04-18 13:30:31 +0200

    Ex3, a

--- src/main/kotlin/com/paulienvanalst/rugbymatch/events/GameEvents.kt
+++ src/main/kotlin/com/paulienvanalst/rugbymatch/events/GameEvents.kt
@@ -6,6 +6,7 @@ import com.paulienvanalst.rugbymatch.game.SetPiece
 import com.paulienvanalst.rugbymatch.game.Type
 import com.paulienvanalst.rugbymatch.team.TeamName
 import org.springframework.context.ApplicationEvent
+import org.springframework.context.event.EventListener
 
 sealed class SetPieceEvent(source: Any, val setPiece: SetPiece, val winningTeam: TeamName) : ApplicationEvent(source)
 
--- src/main/kotlin/com/paulienvanalst/rugbymatch/game/ScoringBoard.kt
+++ src/main/kotlin/com/paulienvanalst/rugbymatch/game/ScoringBoard.kt
@@ -1,20 +1,34 @@
 package com.paulienvanalst.rugbymatch.game
 
+import com.paulienvanalst.rugbymatch.events.ScoringEvent
+import com.paulienvanalst.rugbymatch.events.StartGame
 import com.paulienvanalst.rugbymatch.team.NotImplementedException
+import com.paulienvanalst.rugbymatch.team.TeamName
+import org.springframework.context.event.EventListener
 import org.springframework.stereotype.Component
 
 @Component
 open class ScoringBoard {
 
+    lateinit var hostingTeam: TeamName
+    lateinit var visitingTeam: TeamName
     private var scoringHistory : List<Score> = emptyList()
 
-    fun currentScore(): GameScore {
-        throw NotImplementedException()
-    }
+    fun currentScore() = scoringHistory.getGameScore(hostingTeam, visitingTeam)
 
     fun clear () {
         scoringHistory = emptyList()
+    }
 
+    @EventListener
+    fun handleGameStart(event: StartGame) {
+        hostingTeam = event.hostingTeam
+        visitingTeam = event.visitingTeam
+    }
+
+    @EventListener
+    fun handleScoring(event: ScoringEvent) {
+        scoringHistory += Score(event.team, event.type)
     }
 }
 
